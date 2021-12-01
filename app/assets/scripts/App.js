1ebfd97c5a140106ec23509da9aa55203479b8ad
xromium <r.b.ivanov@mail.ru>
2021-12-01 15:44:43 +0300

    Completed the reveal-on-scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
+
+new RevealOnScroll(document.querySelectorAll(".overview__item"), 75)
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 80)
+new RevealOnScroll(document.querySelectorAll(".site-header__btn-consult"), 210)
 
 let mobileMenu = new MobileMenu()
 
