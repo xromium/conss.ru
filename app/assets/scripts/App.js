13863d4c0d675d45535759109aad0610c31eeb72
xromium <r.b.ivanov@mail.ru>
2021-12-06 11:15:05 +0300

    smth done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,14 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
+import StickyHeader from "./modules/StickyHeader"
+import TogglePills from "./modules/TogglePills"
+
+let stickyHeader = new StickyHeader()
+let togglePills = new TogglePills()
 
 new RevealOnScroll(document.querySelectorAll(".overview__item"), 75)
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 80)
-new RevealOnScroll(document.querySelectorAll(".site-header__btn-consult"), 210)
 
 let mobileMenu = new MobileMenu()
 
